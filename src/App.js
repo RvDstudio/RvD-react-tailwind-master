6a6f891a11ba9241a1849c76a2bea1004261b29e
RvDstudio <rvdstudionl@gmail.com>
2020-06-04 23:18:22 +0200

    Edited indexjs + theme.ss + App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import React from 'react'
 function App() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-6xl">Hello World</h1>
+      <h1 className="text-6xl">RvD:React/Tailwind</h1>
       <p className="text-2xl mb-3">
         This is your React / Tailwind css starter by RvDesignStudio
       </p>
-      <div class="flex bg-gray-200">
-        <div class="flex-initial text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-          Short
+      <div class="flex bg-gradient-green p-5">
+        <div class="flex-1 text-white text-center green-card px-4 py-4 m-2">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
+            amet, ad corrupti numquam culpa reiciendis vitae harum autem ratione
+            veritatis.
+          </p>
         </div>
-        <div class="flex-initial text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-          Medium length
+        <div class="flex-1 text-white text-center green-card px-4 py-4 m-2">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
+            amet, ad corrupti numquam culpa reiciendis vitae harum autem ratione
+            veritatis.
+          </p>
         </div>
-        <div class="flex-initial text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui ad
-          labore ipsam, aut rem quo repellat esse tempore id, quidem
+        <div class="flex-1 text-white text-center green-card px-4 py-4 m-2">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
+            amet, ad corrupti numquam culpa reiciendis vitae harum autem ratione
+            veritatis.
+          </p>
         </div>
       </div>
     </div>
