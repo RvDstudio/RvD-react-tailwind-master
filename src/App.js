48cdf7da72422bb0246857354c1e65fc09b62b21
RvDstudio <rvdstudionl@gmail.com>
2020-06-04 23:26:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ function App() {
   return (
     <div className="container mx-auto">
       <h1 className="text-6xl">RvD:React/Tailwind</h1>
-      <p className="text-2xl mb-3">
+      <p className="text-2xl mb-6">
         This is your React / Tailwind css starter by RvDesignStudio
       </p>
-      <div class="flex bg-gradient-green p-5">
-        <div class="flex-1 text-white text-center green-card px-4 py-4 m-2">
+      <div className="flex bg-gradient-green p-5">
+        <div className="flex-1 text-white text-center green-card px-4 py-4 m-2">
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
             amet, ad corrupti numquam culpa reiciendis vitae harum autem ratione
             veritatis.
           </p>
         </div>
-        <div class="flex-1 text-white text-center green-card px-4 py-4 m-2">
+        <div className="flex-1 text-white text-center green-card px-4 py-4 m-2">
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
             amet, ad corrupti numquam culpa reiciendis vitae harum autem ratione
             veritatis.
           </p>
         </div>
-        <div class="flex-1 text-white text-center green-card px-4 py-4 m-2">
+        <div className="flex-1 text-white text-center green-card px-4 py-4 m-2">
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
             amet, ad corrupti numquam culpa reiciendis vitae harum autem ratione
