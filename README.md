f413c359b9aa2f7c297ecec8cda8ac3278686b7e
RvDstudio <rvdstudionl@gmail.com>
2020-06-04 22:15:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# RvDesignStudio: React & Tailwind CSS
+
+# Install dependencies
+
+npm install
+
+# Watch CSS & run server on :3000
+
+npm start
+
+# Build CSS and assets
+
+npm run build
+
+```
+
+```
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-6xl">Hello World</h1>
+      <p className="text-2xl mb-3">
+        This is your React / Tailwind css starter by RvDesignStudio
+      </p>
+      <div class="flex bg-gray-200">
+        <div class="flex-initial text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+          Short
+        </div>
+        <div class="flex-initial text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+          Medium length
+        </div>
+        <div class="flex-initial text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui ad
+          labore ipsam, aut rem quo repellat esse tempore id, quidem
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
